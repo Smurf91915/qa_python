1e9535c8cb7e09ae3e0d3f7ffd9fdbb14afe6b9d
Nadya Smurova <Smurf91915@yandex.ru>
2022-07-11 01:13:48 +0300

    qa_python sprint 2

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # qa_python
+1. Тест test_add_new_book_add_two_books(self) - Добавление двух книг
+2. Тест test_add_new_book_add_one_book_book_is_added(self) - Добавление одной книги
+3. Тест test_add_new_book_re_add_book_book_not_added(self) - Добавление одной и той же книги повторно
+4. тест test_set_book_rating_not_added_book_rating_not_added(self) - Нельзя добавить рейтинг книге, которой нет в списке
+5. Тест test_set_books_rating_zero_rating_rating_not_added(self) - Нельзя добавить рейтинг книге меньше 1
+6. Тест test_set_books_rating_eleven_rating_rating_not_added(self) - Нельзя добавить рейтинг книге выше 10
+7. Тест test_get_book_rating_not_added_book_rating_not_added(self) - Отсутствие рейтинга у не добавленной книги
+8. Тест test_get_books_with_specific_rating_list_five_rating_list_of_books_rated_five(self) - Вывод списка книг с определенным рейтингом
+9. Тест test_get_books_rating_book_list_returns_a_list_of_books(self) - Вывод списка книг
+10. Тест test_add_book_in_favorites_added_book_book_is_added(self) - Добавление книги в избранное
+11. Тест test_add_book_in_favorites_not_added_book_book_not_added(self) - Нельзя добавить книгу в избранное, если её нет в словаре books_rating
+12. Тест test_delete_book_from_favorites_book_added_to_favorites_the_book_is_deleted(self) - Удаление книги из избранного
+13. Тест test_get_list_of_favorites_books_book_list_returns_a_list_of_favorite_books(self) - Вывод списка избранных книг
--- tests.py
+++ tests.py
@@ -22,3 +22,109 @@ class TestBooksCollector:
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    # Добавление книги
+    def test_add_new_book_add_one_book_book_is_added(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Маленький принц')
+
+        assert len(collector.get_books_rating()) == 1
+
+    # Добавление одной и той же книги повторно
+    def test_add_new_book_re_add_book_book_not_added(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Искусство любить')
+        collector.add_new_book('Искусство любить')
+
+        assert len(collector.get_books_rating()) == 1
+
+    # Нельзя добавить рейтинг книге, которой нет в списке
+    def test_set_book_rating_not_added_book_rating_not_added(self):
+        collector = BooksCollector()
+
+        collector.set_book_rating('Пять языков любви', 5)
+
+        assert len(collector.get_books_rating()) == 0
+
+    # Нельзя добавить рейтинг книге меньше 1
+    def test_set_books_rating_zero_rating_rating_not_added(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Два капитана')
+        collector.set_book_rating('Два капитана', 0)
+
+        assert collector.get_book_rating('Два капитана') == 1
+
+    # Нельзя добавить рейтинг книге выше 10
+    def test_set_books_rating_eleven_rating_rating_not_added(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Капитанская дочка')
+        collector.set_book_rating('Капитанская дочка', 11)
+
+        assert collector.get_book_rating('Капитанская дочка') == 1
+
+    # Отсутствие рейтинга у не добавленной книги
+    def test_get_book_rating_not_added_book_rating_not_added(self):
+        collector = BooksCollector()
+
+        assert collector.get_book_rating('Принцесса на горошине') == None
+
+    # Вывод списка книг с определенным рейтингом
+    def test_get_books_with_specific_rating_list_five_rating_list_of_books_rated_five(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Человек, который принял жену за шляпу')
+        collector.add_new_book('Горе от ума')
+        collector.set_book_rating('Горе от ума', 5)
+
+        assert collector.get_books_with_specific_rating(5)[0] == 'Горе от ума' and len(collector.get_books_with_specific_rating(5)) == 1
+
+    # Вывод списка книг
+    def test_get_books_rating_book_list_returns_a_list_of_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Властелин овец')
+        collector.add_new_book('Хоббит туда-сюда')
+
+        assert len(collector.get_books_rating()) == 2
+
+    # Добавление книги в избранное
+    def test_add_book_in_favorites_added_book_book_is_added(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Продавец воздуха')
+        collector.add_new_book('Власть тьмы')
+        collector.add_book_in_favorites('Продавец воздуха')
+
+        assert collector.get_list_of_favorites_books()[0] == 'Продавец воздуха' and len(collector.get_list_of_favorites_books()) == 1
+
+    # Нельзя добавить книгу в избранное, если её нет в словаре books_rating
+    def test_add_book_in_favorites_not_added_book_book_not_added(self):
+        collector = BooksCollector()
+
+        collector.add_book_in_favorites('Как управлять рабами')
+
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+    # Удаление книги из избранного
+    def test_delete_book_from_favorites_book_added_to_favorites_the_book_is_deleted(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Мой дедушка был вишней')
+        collector.add_book_in_favorites('Мой дедушка был вишней')
+        collector.get_list_of_favorites_books() == 1
+        collector.delete_book_from_favorites('Мой дедушка был вишней')
+
+        assert  len(collector.get_list_of_favorites_books()) == 0
+
+    # Вывод списка избранных книг
+    def test_get_list_of_favorites_books_book_list_returns_a_list_of_favorite_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Сочные глазки и пижама в лягушечку')
+        collector.add_book_in_favorites('Сочные глазки и пижама в лягушечку')
+
+        assert len(collector.get_list_of_favorites_books()) == 1
